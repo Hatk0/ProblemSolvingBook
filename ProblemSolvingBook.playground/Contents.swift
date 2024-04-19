15af916c09ca440c4cbdc7e73d7a551cc0caa257
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-04-19 20:12:48 +0300

    Third task

--- ProblemSolvingBook.playground/Contents.swift
+++ ProblemSolvingBook.playground/Contents.swift
@@ -14,3 +14,16 @@ let hairColor: String = "Брюнет"
 var height: Int = 187
 var weight: Double = 85.5
 var shoeSize: Float = 42
+
+/// **№3. Добавление комментариев**
+print("Производитель: \(manufacturer), Модель: \(model), Процессор: \(processor), ОЗУ или объединенная память: \(operatingMemory), Размер экрана: \(screenSize)")
+print("--------------------------------------------")
+
+print("""
+Цвет глаз: \(eyeColor)
+Цвет волос: \(hairColor)
+Вес: \(weight)
+Рост: \(height)
+Размер обуви: \(shoeSize)
+""")
+print("--------------------------------------------")
