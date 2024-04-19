8f5867d18a06ce4ac6552bfe64b36258486d5902
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-04-19 20:12:28 +0300

    Second task

--- ProblemSolvingBook.playground/Contents.swift
+++ ProblemSolvingBook.playground/Contents.swift
@@ -7,3 +7,10 @@ var model: String = "MacBook Pro"
 var processor: String = "M2 Pro"
 let operatingMemory: Int = 16
 var screenSize: Double = 14.2
+
+/// **№2. Описание внешности**
+let eyeColor: String = "Карий"
+let hairColor: String = "Брюнет"
+var height: Int = 187
+var weight: Double = 85.5
+var shoeSize: Float = 42
