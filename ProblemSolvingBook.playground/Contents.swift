b9b963b066fee3543c8a0573f36d412c829efdd2
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-04-19 20:11:40 +0300

    First task

--- ProblemSolvingBook.playground/Contents.swift
+++ ProblemSolvingBook.playground/Contents.swift
@@ -1,3 +1,9 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+/// **ТРЕНИРОВКА**
+/// **№1. Описание ноутбука**
+let manufacturer: String = "Apple"
+var model: String = "MacBook Pro"
+var processor: String = "M2 Pro"
+let operatingMemory: Int = 16
+var screenSize: Double = 14.2
