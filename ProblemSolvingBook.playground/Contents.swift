6ee4cec4f8c7b6d0622fc8692c08c1c4297ef0cc
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-05-03 18:00:29 +0300

    113 task

--- ProblemSolvingBook.playground/Contents.swift
+++ ProblemSolvingBook.playground/Contents.swift
@@ -1641,3 +1641,48 @@ if let convertedAmount = convertExchangeRates(sum: 100,
     print("Результат обмена: \(convertedAmount)")
 }
 print("--------------------------------------------")
+
+/// **№113. Машина времени**
+enum TimePeriod {
+    case precambrianTime
+    case paleozoicEra
+    case mesozoicEra
+    case cenozoicEra
+    
+    func chooseTimePeriod() -> String {
+        switch self {
+        case .precambrianTime:
+            return """
+Precambrian Time started at the beginning of the Earth 4.6 billion years ago. For billions of years, there was no life on the planet. It wasn't until the end of Precambrian Time that single-celled organisms came into existence. No one is certain how life on Earth began, but theories include the Primordial ​Soup Theory, Hydrothermal Vent Theory, and Panspermia Theory.
+
+The end of this time span saw the rise of a few more complex animals in the oceans, such as jellyfish. There was still no life on land, and the atmosphere was just beginning to accumulate the oxygen required for higher-order animals to survive. Living organisms wouldn't proliferate and diversify until the next era.
+"""
+        case .paleozoicEra:
+            return """
+The Paleozoic Era began with the Cambrian Explosion, a relatively rapid period of speciation that kicked off a long period of life flourishing on Earth. Vast amounts of life forms from the oceans moved onto the land. Plants were the first to make the move, followed by invertebrates. Not long afterward, vertebrates took to the land. Many new species appeared and thrived.
+
+The end of the Paleozoic Era came with the largest mass extinction in the history of life on Earth, wiping out 95% of marine life and nearly 70% of life on land. Climate changes were most likely the cause of this phenomenon as the continents all drifted together to form Pangaea. As devastating this mass extinction was, it paved the way for new species to arise and a new era to begin.
+"""
+        case .mesozoicEra:
+            return """
+After the Permian Extinction caused so many species to go extinct, a wide variety of new species evolved and thrived during the Mesozoic Era, which is also known as the "age of the dinosaurs" since dinosaurs were the dominant species of the age.
+
+The climate during the Mesozoic Era was very humid and tropical, and many lush, green plants sprouted all over the Earth. Dinosaurs started off small and grew larger as the Mesozoic Era went on. Herbivores thrived. Small mammals came into existence, and birds evolved from the dinosaurs.
+
+Another mass extinction marked the end of the Mesozoic Era, whether triggered by a giant meteor or comet impact, volcanic activity, more gradual climate change, or various combinations of these factors. All the dinosaurs and many other animals, especially herbivores, died off, leaving niches to be filled by new species in the coming era.
+"""
+        case .cenozoicEra:
+            return """
+The final time period on the Geologic Time Scale is the Cenozoic Period. With large dinosaurs now extinct, smaller mammals that had survived were able to grow and become dominant.
+
+The climate changed drastically over a relatively short period of time, becoming much cooler and drier than during the Mesozoic Era. An ice age covered most temperate parts of the Earth with glaciers, causing life to adapt relatively rapidly and the rate of evolution to increase.
+
+All species of life—including humans—evolved into their present-day forms over the course of this era, which hasn't ended and most likely won't until another mass extinction occurs.
+"""
+        }
+    }
+}
+
+let period: TimePeriod = .precambrianTime
+print(period.chooseTimePeriod())
+print("--------------------------------------------")
